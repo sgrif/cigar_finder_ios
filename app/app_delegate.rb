d4cb21a2e5eb978263a73ea4a095ae1d5ed5efa8
sgrif <sean@thoughtbot.com>
2013-04-25 22:10:43 -0600

    Search form

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,5 @@
-class AppDelegate
-  def application(application, didFinishLaunchingWithOptions:launchOptions)
-    true
+class AppDelegate < ProMotion::AppDelegateParent
+  def on_load(app, options)
+    open SearchFormScreen.new
   end
 end
