a0db1a8d013e2a98d6b3accc72b1dae8d809c6ee
sgrif <sean@thoughtbot.com>
2013-05-03 11:37:15 -0600

    Finish layout of detail view

--- app/presenters/search_result_presenter.rb
+++ app/presenters/search_result_presenter.rb
@@ -10,4 +10,18 @@ class SearchResultPresenter < Struct.new(:search_result)
   def carried_past_tense
     search_result.carried? ? 'carried' : 'not carried'
   end
+
+  def last_updated_number
+    last_updated_description[/^\d+/]
+  end
+
+  def last_updated_words
+    last_updated_description[/[^\d]+$/].lstrip
+  end
+
+  private
+
+  def last_updated_description
+    search_result.updated_at.stringWithTimeDifference
+  end
 end
--- app/views/last_reported_view.rb
+++ app/views/last_reported_view.rb
@@ -10,11 +10,27 @@ class LastReportedView < UIView
   def layoutSubviews
     Motion::Layout.new do |layout|
       layout.view self
-      layout.subviews 'description' => description
-      layout.horizontal '|-[description]-|'
+      if search_result.carried.nil?
+        layout.subviews 'description' => description
+        layout.horizontal '|-[description]-|'
+        description.preferredMaxLayoutWidth = bounds.size.width - 40
+      else
+        layout.subviews 'description' => description, 'block' => updated_at_container
+        layout.horizontal '|-[description]-10-[block(==80)]|'
+        layout.vertical '|[block]|'
+
+        Motion::Layout.new do |layout|
+          layout.view updated_at_container
+          layout.subviews 'number' => updated_at_number, 'words' => updated_at_words
+          layout.metrics 'top' => '>=14', 'bottom' => '>=14'
+          layout.horizontal '|[number]|'
+          layout.horizontal '|[words]|'
+          layout.vertical '|-[number][words]-|'
+        end
+        description.preferredMaxLayoutWidth = bounds.size.width - 120
+      end
       layout.vertical '|-[description]-|'
     end
-    description.preferredMaxLayoutWidth = bounds.size.width - 40
     super
   end
 
@@ -26,6 +42,32 @@ class LastReportedView < UIView
       label.backgroundColor = :clear.uicolor
       label.textColor = '#F6F4F3'.to_color
       label.font = UIFont.systemFontOfSize(12)
+    end
+  end
+
+  def updated_at_container
+    @updated_at_container ||= UIView.new.tap do |view|
+      view.backgroundColor = '#483C31'.to_color
+    end
+  end
+
+  def updated_at_number
+    @updated_at_number ||= UILabel.new.tap do |label|
+      label.text = search_result_presenter.last_updated_number
+      label.textColor = '#F6F4F3'.to_color
+      label.textAlignment = :center.uitextalignment
+      label.font = UIFont.systemFontOfSize(25)
+      label.backgroundColor = :clear.uicolor
+    end
+  end
+
+  def updated_at_words
+    @updated_at_words ||= UILabel.new.tap do |label|
+      label.text = search_result_presenter.last_updated_words.upcase
+      label.textColor = BW.rgba_color(0xF6, 0xF4, 0xF3, 0.5)
+      label.textAlignment = :center.uitextalignment
+      label.font = UIFont.systemFontOfSize(12)
+      label.backgroundColor = :clear.uicolor
     end
   end
 
