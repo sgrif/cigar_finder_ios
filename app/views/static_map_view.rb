133d4e29b6495aa3b51185a7b3b83b9ef7e320d4
sgrif <sean@thoughtbot.com>
2013-05-03 17:50:19 -0600

    Adjust map scale on retina

--- app/views/static_map_view.rb
+++ app/views/static_map_view.rb
@@ -7,10 +7,18 @@ class StaticMapView < UIImageView
   end
 
   def update_image
+    if Device.retina?
+      scale = 2
+      icon = "http://s3.amazonaws.com/cigar-finder/map_marker_ios.png"
+    else
+      scale = 1
+      icon = "http://s3.amazonaws.com/cigar-finder/map_marker_ios.png"
+    end
     setImageWithURL(NSURL.URLWithString("http://maps.googleapis.com/maps/api/staticmap?sensor=true" +
       "&size=#{frame.size.width.to_i}x#{frame.size.height.to_i}" +
+      "&scale=#{scale}" +
       "&key=AIzaSyDWBzeBlZ7hC_NUDIP6WsmpDXXmhzgLdVk" +
-      "&markers=icon:http://s3.amazonaws.com/cigar-finder/map_marker_ios.png%7C#{location.latitude}%2C#{location.longitude}"
+      "&markers=icon:#{icon}%7C#{location.latitude}%2C#{location.longitude}"
       ))
   end
 end
